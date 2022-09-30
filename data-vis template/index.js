c7abe88dac2ec68c37b3d869614b444023e9a15b
Oswaldo Salazar <68619634+FoxMulder1994@users.noreply.github.com>
2022-09-30 10:05:14 -0500

    Create index.js

---
+++ data-vis template/index.js
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Case study 2: Data visualisation</title>
+
+    <!-- Libraries -->
+    <script src="https://unpkg.com/mappa-mundi@0.0.5/dist/mappa.js" type="text/javascript"></script>
+    <script src="lib/p5.min.js"></script>
+    <script src="lib/tf.min.js"></script>
+    
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" />
+    <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"></script>
+      
+      
+      
+    <!-- p5 -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.0.0/addons/p5.sound.min.js"></script>
+    <!-- ml5 -->
+   <script src="https://unpkg.com/ml5@0.6.0/dist/ml5.min.js"></script>
+      
+    <link rel="stylesheet" href="style.css">
+
+    <!-- Main sketch file -->
+    <script src="sketch.js"></script>
+
+    <!-- Main visualisation files -->
+    <script src="tech-diversity-race.js"></script>
+    <script src="tech-diversity-gender.js"></script>
+    <script src ="Venezuela_VitaminA.js"></script>
+    <script src ="Venezuela_Nutrients.js"></script>   
+    <script src ="World_PSA.js"></script>
+    
+     
+      
+    <meta charset="utf-8" />
+    
+    <script src="pay-gap-by-job-2017.js"></script>
+    <script src="pay-gap-1997-2017.js"></script>
+    <script src="climate-change.js"></script>
+    
+    <!-- Add extra scripts below -->
+    <script src="helper-functions.js"></script>
+    <script src="gallery.js"></script>
+    <script src="pie-chart.js"></script>
+  </head>
+  <body>
+    <div id="app" class="container">
+      <ul id="visuals-menu"></ul>
+    </div>
+  </body>
+</html>
